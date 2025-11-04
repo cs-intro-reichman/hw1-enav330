76c5d856fb1b1fde7d9d1a8cfc6c6981ff9efa65
Enav Gefen <enav.gefen@post.runi.ac.il>
2025-11-04 17:43:12 +0200

    fixed assignment

--- FVCalc.java
+++ FVCalc.java
@@ -8,6 +8,6 @@ public class FVCalc {
 	double double_rate =Double.parseDouble(rate)*0.01;
 	double double_n=Double.parseDouble(n);
 	double futureValue = double_current_value*(Math.pow(1+double_rate,double_n));
-	System.out.println("After " + n + " years, $ " + double_current_value + " saved at " + rate +"% will yield $" +"" + (int)futureValue);
+	System.out.println("After " + n + " years, $" + double_current_value + " saved at " + rate +"% will yield $" +"" + (int)futureValue);
 	} 
 }
